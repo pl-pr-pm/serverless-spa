da41d2ae845d166bba6ecc7a9aa5ba65af908cd1
EC2 Default User <ec2-user@ip-10-0-0-10.ap-northeast-1.compute.internal>
2020-12-29 08:40:59 +0000

    reset

---
+++ getImageById/get-image-by-id.py
@@ -0,0 +1,84 @@
+import boto3
+import uuid
+import json
+import logging
+import os
+import datetime
+import decimal
+from botocore.exceptions import ClientError
+
+"""Loggerをセット"""
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            if obj % 1 > 0:
+                return float(obj)
+            else:
+                return int(obj)
+
+        return super(DecimalEncoder, self).default(obj)
+
+"""
+Dynamoインスタンスを作成
+Lambda関数のコンテナが再利用された際に置ける処理効率向上の為に、
+接続処理をLambda関数のエントリポイントとなるLambda_handler関数内に記述するのではなく、
+グローバルスコープで記述とする
+"""
+dynamodb = boto3.resource('dynamodb', region_name = 'ap-northeast-1')
+table = dynamodb.Table(os.getenv('TABLE_NAME'))
+    
+def lambda_handler(event, context):
+    try:
+        photo_id = event['pathParameters']['id']
+        try:
+            response = table.get_item(
+                Key = {
+                    'photo_id':photo_id
+                }
+            )
+            
+            logger.info(response)
+            
+            if 'Item' not in response:
+                logging.info("Specified key is not found.")
+                response = {
+                    'statusCode':'404',
+                    'body':'Not Found',
+                    'headers': {
+                        'Content-Type' : 'application/json',
+                        'Access-Control-Allow-Origin':'*'
+                    },
+                }
+                return response
+        except ClientError as e:
+            logging.info(e.response['Error']['Message'])
+            response = {
+                'statusCode':'400',
+                'body':e.response['Error']['Message'],
+                'headers':{
+                        'Content-Type':'application/json',
+                        'Access-Control-Allow-Origin':'*'
+                    },
+                }
+            return response
+                
+        else:
+            item = json.dumps(response['Item'], cls=DecimalEncoder)
+                
+            response = {
+                'statusCode':'200',
+                'body':item,
+                'headers':{
+                    'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*'
+                    },
+                }
+                
+            return response
+            
+    except Exception as e:
+        logging.error("type: %s", type(e))
+        logging.error(e)
